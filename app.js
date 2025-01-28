073bf798949b99bff6b631b3509e6aa34a578291
gerry <gerryyhonatan29@gmail.com>
2025-01-29 00:30:19 +0700

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-
 /*
 // menuliskan string ke file (synchronus)
 fs.writeFileSync('test.txt', 'Hello World');
@@ -18,36 +16,14 @@ fs.readFile('data/test.txt', 'utf-8', (err,data) => {
 });
 */
 
-// Readline
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const { tulisPertanyaan, simpanContact } = require("./contacts")
 
-// membuat folder data jika belum ada
-const dirPath = './data'
-if(!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath)
+const main = async() => {
+    const nama = await tulisPertanyaan('Masukkan Nama Anda : ')
+    const email = await tulisPertanyaan('Masukkan Email Anda : ')
+    const noHP = await tulisPertanyaan('Masukkan No HP Anda : ')
+
+    simpanContact(nama, email, noHP)
 }
 
-// membuat file contacts.json jika belum ada
-const dataPath = './data/contacts.json'
-
-rl.question('Masukkan Nama Anda : ', (nama) => {
-    rl.question('Masukkan No Hp Anda : ', (noHP) => {
-        const contact = {
-            nama: nama,
-            noHP : noHP
-        }
-        const file = fs.readFileSync('data/contacts.json', 'utf-8');
-        const contacts = JSON.parse(file)
-        
-        contacts.push(contact)
-
-        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
-
-        console.log('Terimakasih sudah memasukkan data');
-        rl.close();
-    })
-});
+main()
